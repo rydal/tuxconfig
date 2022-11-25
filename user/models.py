2ce18f238e6846323f85c0893d15373a78457db6
Rob <rob@rydal.org>
2022-11-25 18:56:30 +0000

    recaptcha added

--- user/models.py
+++ user/models.py
@@ -7,16 +7,6 @@ from django.db.models import UniqueConstraint
 
 class RequestedDeviceId(models.Model):
     device_id = models.CharField(max_length=9, primary_key=True)
-    version_number = models.SmallIntegerField(default=0)
-    vote_count = models.PositiveIntegerField(default=0)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    objects = models.Manager()
-    
-        
-class DownloadedDeviceId(models.Model):
-    device_id = models.CharField(max_length=9, primary_key=True)
-    version_number = models.SmallIntegerField(default=0)
     vote_count = models.PositiveIntegerField(default=0)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- user/views.py
+++ user/views.py
@@ -2,10 +2,14 @@ import json
 import operator
 from urllib.request import urlopen
 
+import certifi
+import urllib3
 from django.shortcuts import render
 
 from django.db import models
 import requests
+from urllib3 import request
+
 # Create your models here.
 from contributor.models import RepoModel, Devices
 from tuxconfig_django import settings
@@ -16,32 +20,41 @@ import httplib2
 import ast
 
 from user.forms import DownloadedIdForm
+from user.models import RequestedDeviceId
 
 
 def check_device_exists(request,device_id):
     if device_id is None:
         return JsonResponse({"error" : "device id not set"})
+    if "recaptcha_token" not in request.POST:
+        return JsonResponse({"error": "No recaptcha token sent"})
+    elif get_recaptcha_token(request.POST['recaptcha_token'],get_client_ip(request)) is False:
+        return JsonResponse({"error": "Invalid recaptcha token sent"})
+    else:
+        device_already, created = RequestedDeviceId.objects.update_or_create(device_id=device_id)
+        if device_already is not None and created is True:
+            device_already.vote_count = device_id.vote_count + 1
+            device_already.save()
+        devices = Devices.objects.filter(device_id=device_id).select_related()
+        repositories = []
+        for device in devices:
+             repositories.append(device.repo_model)
+        if len(repositories) == 0:
+            return JsonResponse({'none' : True })
+        repositories_available = []
 
-    devices = Devices.objects.filter(device_id=device_id).select_related()
-    repositories = []
-    for device in devices:
-         repositories.append(device.repo_model)
-    if len(repositories) == 0:
-        return JsonResponse({'none' : True })
-    repositories_available = []
-
-    for result in repositories:
-        if settings.CHECK_REPO_STILL_ON_GITHUB:
-            clone_url = "https://github.com/" + result.git_username + "/"  + result.git_repo + "/commit/" + result.git_commit
-            h = httplib2.Http()
-            resp = h.request(clone_url, 'HEAD')
-            if int(resp[0]['status']) < 400:
-                repositories_available.append({"clone_url" : clone_url, "stars" : str(result.stars),"pk" : result.id })
-        else:
-            repositories_available.append({"clone_url" : result.clone_url, "stars" : str(result.stars),"pk" : result.id })
-    s = json.dumps(repositories_available)
-    s = ast.literal_eval(s)
-    return JsonResponse(s,safe=False)
+        for result in repositories:
+            if settings.CHECK_REPO_STILL_ON_GITHUB:
+                clone_url = "https://github.com/" + result.git_username + "/"  + result.git_repo + "/commit/" + result.git_commit
+                h = httplib2.Http()
+                resp = h.request(clone_url, 'HEAD')
+                if int(resp[0]['status']) < 400:
+                    repositories_available.append({"clone_url" : clone_url, "stars" : str(result.stars),"pk" : result.id })
+            else:
+                repositories_available.append({"clone_url" : result.clone_url, "stars" : str(result.stars),"pk" : result.id })
+        s = json.dumps(repositories_available)
+        s = ast.literal_eval(s)
+        return JsonResponse(s,safe=False)
 
 
 
@@ -59,5 +72,24 @@ def get_user_details(request,repo_model):
     return render(request,"get_user_details.html", {"git_user" : respBody, "model" : model, "form": downloaded_from })
 
 
+def get_recaptcha_token(token,remote_ip):
+        http = urllib3.PoolManager(ca_certs=certifi.where())
+        recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify'
+        payload = {
+            'secret': settings.RECAPTCHA_PRIVATE_KEY,
+            'response': token,
+            'remoteip': remote_ip,
+        }
+        encoded_data = json.dumps(payload).encode('utf-8')
+        req = http.request('POST', recaptcha_url, payload=encoded_data,headers={'Content-Type': 'application/json'})
 
+        result = json.loads(req.data.decode('utf-8'))['json']
+        return result.get('success', False)
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('X-Forwarded-For')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
